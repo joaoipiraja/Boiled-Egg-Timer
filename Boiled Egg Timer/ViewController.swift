e24c45854bb08f8bcf65343501f98ab3a233fde0
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2020-12-20 10:56:26 -0300

    Initial Commit

---
+++ Boiled Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Boiled Egg Timer
+//
+//  Created by João Victor Ipirajá de Alencar on 20/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Boiled Egg TimerTests/Boiled_Egg_TimerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Boiled_Egg_TimerTests.swift
+//  Boiled Egg TimerTests
+//
+//  Created by João Victor Ipirajá de Alencar on 20/12/20.
+//
+
+import XCTest
+@testable import Boiled_Egg_Timer
+
+class Boiled_Egg_TimerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
